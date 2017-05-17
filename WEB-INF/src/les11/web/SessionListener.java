a1fae5fd55ee2fbce6da51e7d40c9ca2952feb33
yauhenav <enavitski@gmail.com>
2017-05-17 19:21:16 +0300

    brushed up some files

---
+++ WEB-INF/src/les11/web/SessionListener.java
@@ -0,0 +1,22 @@
+package les11.web;
+
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSession;
+
+import les11.logic.controller.*;
+
+/**
+ * Created by yauhenav on 9.5.17.
+ */
+public class SessionListener implements HttpSessionListener {
+    Service sesMngObj;
+    public void sessionCreated(HttpSessionEvent e) {
+        HttpSession sessObj = e.getSession();
+        sesMngObj = new Service();
+        sessObj.setAttribute("sessionObject", sesMngObj);
+    }
+    public void sessionDestroyed(HttpSessionEvent e) {
+        sesMngObj.close();
+    }
+}
